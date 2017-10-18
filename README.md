d441c3cbbd1e750a591548446a2070dcf5448c78
Marcus Nunes <marcus.nunes@gmail.com>
2017-10-18 11:18:50 -0300

    Atualização geral
    Limpeza do código, contrução de gráficos e atualização do banco de dados

--- README.md
+++ README.md
@@ -2,11 +2,17 @@
 
 Eu me achava gordo e decidi começar uma dieta em 2016. Como anotei meu consumo calórico e meu peso diariamente a partir de 17 de fevereiro, eu disponho de uma quantidade de dados razoável para análise.
 
-O arquivo codigo.r possui a rotina que utilizei para gerar o gráfico do meu desempenho, bem como os modelos estatísticos que ajustei a estes dados.
+O arquivo `codigo.r` possui a rotina que utilizei para gerar os gráficos do meu desempenho, bem como o modelo estatístico que ajustei a estes dados.
 
-O arquivo data.csv possui meus dados.
+O arquivo `data.csv` possui meus dados de peso e consumo calórico, sendo que estes estão desatualizados.
 
-O arquivo .pdf tem um exemplo de gráfico que pode ser obtido com meu código. Perceba que ele possui uma linha chamada App, que é uma referência sobre o peso que eu teria se estivesse conseguindo emagrecer 500g por semana como o MyFitnessPal prometeu.
+O arquivo `grafico01.pdf` tem um exemplo de gráfico que pode ser obtido com meu código. Note que ele possui duas tendências de evolução do peso (Curto e Longo Prazos) e o peso real.
 
-Utilize o tutorial disponível em https://lanroth.com/exportMyFitnessPal.html para expertar seus próprios resultados.
+Também é possível gerar os boxplots mensais do peso. O resultado pode ser visto em `grafico02.pdf`.
+
+Reportei estatísticas descritivas a respeito da evolução do meu peso nestes meses.
+
+Por fim, ajustei um modelo de previsão do peso futuro utilizando séries temporais. Este gráficos ainda não foi implementado utilizando o pacote `ggplot2`.
+
+Utilize o tutorial disponível em https://lanroth.com/exportMyFitnessPal.html para expertar seus próprios resultados, caso também utilize o MyFitnessPal.
 
---
+++ analise.r
@@ -0,0 +1,122 @@
+library(ggplot2)
+library(dplyr)
+library(forecast)
+library(lubridate)
+library(wesanderson)
+library(scales)
+
+# leitura de dados
+
+dados <- as_data_frame(read.table(file="data.csv", sep=",", header=TRUE))
+dados <- dados %>%
+  select(-Calories)
+
+dim(dados)
+
+str(dados)
+
+dados$Date           <- ymd(dados$Date)
+dados$Year           <- year(dados$Date)
+dados$Month          <- month(dados$Date)
+dados$Day            <- day(dados$Date)
+dados$GrupoDiaSemana <- wday(dados$Date, label=TRUE)
+dados$GrupoMes       <- ymd(paste(dados$Year, dados$Month, "01", sep="-"))
+
+# grafico
+
+g1 <- ggplot(data=dados, aes(x=Date)) + 
+  labs(x="Data", y="Peso (kg)") + 
+  geom_line(aes(y=Weight, colour="Peso Real")) + 
+  geom_smooth(aes(y=Weight, colour="Longo Prazo"), method="loess", se=FALSE, span=0.5) +
+  geom_smooth(aes(y=Weight, colour="Curto Prazo"), method="loess", se=FALSE, span=0.25) + 
+  scale_colour_manual("", values = wes_palette("Zissou")[c(3, 5, 1)]) + 
+  scale_y_continuous(breaks = round(seq(floor(min(dados$Weight)), ceiling(max(dados$Weight)), by=1), 1), limits=c(min(dados$Weight), max(dados$Weight))) + 
+  scale_x_date(breaks=seq(min(dados$Date), max(dados$Date), by="1 month"), date_labels="%b/%Y", minor_breaks=seq(min(dados$Date), max(dados$Date), by="1 month")) +
+  theme_bw() +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+g1
+
+pdf("grafico01.pdf")
+g1
+dev.off()
+
+# analise a cada mes
+
+g2 <- ggplot(data=dados, aes(x=GrupoMes, y=Weight, group=GrupoMes, fill=as.factor(GrupoMes))) +
+  labs(x="Meses", y="Peso (kg)") + 
+  geom_boxplot() + 
+  scale_fill_manual("Meses", values=rep(wes_palette(5, name="Zissou"), 20)[1:length(unique(dados$GrupoMes))]) +
+  scale_y_continuous(breaks = round(seq(floor(min(dados$Weight)), ceiling(max(dados$Weight)), by=1), 1), limits=c(min(dados$Weight), max(dados$Weight))) + 
+  scale_x_date(breaks=seq(min(dados$Date), max(dados$Date), by="1 month"), date_labels="%b/%Y", minor_breaks=seq(min(dados$Date), max(dados$Date), by="1 month")) +
+  theme_bw() +
+  guides(fill=FALSE) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1))
+
+pdf("grafico02.pdf")
+g2
+dev.off()
+
+# analise a cada dia da semana
+
+ggplot(data=dados, aes(x=GrupoDiaSemana, y=Weight, group=GrupoDiaSemana, fill=GrupoDiaSemana)) +
+  labs(x="Dia da Semana", y="Peso (kg)") + 
+  geom_boxplot() + 
+  scale_fill_manual("Dia da Semana", values=rep(wes_palette(3, name="Royal1"), 20)[1:length(unique(dados$GrupoDiaSemana))]) +
+  scale_y_continuous(breaks = round(seq(floor(min(dados$Weight)), ceiling(max(dados$Weight)), by=1), 1), limits=c(min(dados$Weight), max(dados$Weight))) + 
+  guides(fill=FALSE) +
+  theme_bw()
+
+# descritivas
+
+dados %>%
+  group_by(Year, Month) %>%
+  summarise(Mediana=median(Weight), Média=mean(Weight)) %>%
+  mutate(Data=ymd(paste(Year, Month, 01, sep="-"))) %>%
+  select(Data, Média, Mediana) %>%
+  print(n=Inf)
+
+dados %>%
+  arrange(Weight) %>%
+  select(Day, Month, Year, Weight, GrupoDiaSemana) %>%
+  print(n=20)
+
+dados %>%
+  arrange(Weight) %>%
+  select(Day, Month, Year, Weight, GrupoDiaSemana) %>%
+  print(n=20) %>%
+  filter(Weight < 71)
+
+dados %>%
+  arrange(Weight) %>%
+  head(n=20) %>%
+  group_by(Year) %>%
+  count()
+  
+dados %>%
+  arrange(Weight) %>%
+  head(n=50) %>%
+  count(GrupoDiaSemana) %>%
+  arrange(desc(n))
+
+dados %>%
+  filter(Year==2017) %>%
+  arrange(Weight) %>%
+  head(n=30) %>%
+  count(GrupoDiaSemana) %>%
+  arrange(desc(n))
+
+
+
+#######################################
+### analise usando series temporais ###
+#######################################
+
+# arima - melhorar visualização
+
+ajuste.arima <- auto.arima(dados$Weight, max.p=10, max.q=10, max.d=3, seasonal=TRUE, ic="aicc")
+
+tsdiag(ajuste.arima)
+
+plot(forecast(ajuste.arima, h=20))
+
